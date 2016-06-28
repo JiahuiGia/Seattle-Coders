79038d3b087f699768751ec95d7b43ab95980c98
Kuai Yu <yukuairoy@gmail.com>
2016-06-27 23:08:00 -0700

    Add print_level_order. Improve method names. (#4)
    * Added a question.
    
    * Improve method names for better readability.

--- Trees/from_edges_to_tree.py
+++ Trees/from_edges_to_tree.py
@@ -31,7 +31,7 @@ class FindRoot(object):
     def __init__(self, list_of_edges):
         self.list_of_edges = list_of_edges
 
-    def find_root_dict(self):
+    def find_root_using_dict(self):
         is_child = defaultdict(bool)
         for parent, child in self.list_of_edges:
             is_child[parent] = is_child[parent]
@@ -40,7 +40,7 @@ class FindRoot(object):
             if not is_a_child:
                 return node
 
-    def find_root_set(self):
+    def find_root_using_set(self):
         parents, children = set(), set()
         for parent, child in self.list_of_edges:
             parents.add(parent)
@@ -53,7 +53,7 @@ class FindLeaves(object):
     def __init__(self, list_of_edges):
         self.list_of_edges = list_of_edges
 
-    def find_leaves_dict(self):
+    def find_leaves_using_dict(self):
         is_parent = defaultdict(bool)
         for parent, child in self.list_of_edges:
             is_parent[parent] = True
@@ -62,7 +62,7 @@ class FindLeaves(object):
                   if not is_a_parent}
         return leaves
 
-    def find_leaves_set(self):
+    def find_leaves_using_set(self):
         parents, children = set(), set()
         for parent, child in self.list_of_edges:
             parents.add(parent)
@@ -91,14 +91,14 @@ class TreePrinter(object):
             for child in reversed(self.nodes_to_children[current_node]):
                 stack.append((child, current_level + 1))
 
-    def print_level_order(self):
+    def print_level_order_storing_level(self):
         root = self.find_root()
         queue = deque([(root, 0)])
         prev_level = -1
         nodes_in_current_level = []
         while queue:
             current_node, current_level = queue.popleft()
-            if current_level != prev_level:
+            if current_level > 0 and current_level != prev_level:
                 _print_level(nodes_in_current_level)
                 nodes_in_current_level = []
                 prev_level = current_level
@@ -106,6 +106,19 @@ class TreePrinter(object):
             for child in self.nodes_to_children[current_node]:
                 queue.append((child, current_level + 1))
         _print_level(nodes_in_current_level)
+
+    def print_level_order(self):
+        root = self.find_root()
+        queue = deque([(root, 0)])
+        prev_level = -1
+        while queue:
+            current_node, current_level = queue.popleft()
+            if current_level > 0 and current_level != prev_level:
+                print
+                prev_level = current_level
+            print current_node,
+            for child in self.nodes_to_children[current_node]:
+                queue.append((child, current_level + 1))
 
     def get_leaves(self, root):
         if not root:
@@ -119,8 +132,7 @@ class TreePrinter(object):
 
 
 def _print_level(nodes_in_same_level):
-    if nodes_in_same_level:
-        print ' '.join(nodes_in_same_level)
+    print ' '.join(nodes_in_same_level)
 
 
 def _populate_children(list_of_edges):
@@ -141,16 +153,18 @@ if __name__ == '__main__':
     LEAVES = ['flower', 'bird', 'human', 'salmon']
 
     FR = FindRoot(LIST_OF_EDGES)
-    assert FR.find_root_dict() == 'being'
-    assert FR.find_root_set() == 'being'
+    assert FR.find_root_using_dict() == 'being'
+    assert FR.find_root_using_set() == 'being'
 
     FL = FindLeaves(LIST_OF_EDGES)
-    assert FL.find_leaves_dict() == set(LEAVES)
-    assert FL.find_leaves_set() == set(LEAVES)
+    assert FL.find_leaves_using_dict() == set(LEAVES)
+    assert FL.find_leaves_using_set() == set(LEAVES)
 
     TP = TreePrinter(LIST_OF_EDGES)
+    assert TP.get_leaves(TP.find_root()) == LEAVES
     print '**********depth-first traversal**********'
     TP.print_dfs()
-    print '**********breadth-first traversal**********'
+    print '**********breadth-first traversal, NOT storing level**********'
     TP.print_level_order()
-    assert TP.get_leaves(TP.find_root()) == LEAVES
+    print '**********breadth-first traversal, storing level**********'
+    TP.print_level_order_storing_level()
